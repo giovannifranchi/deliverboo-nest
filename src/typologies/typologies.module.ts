4736b84b174981e57aac72b2715307c989fdc56b
giovannifranchi <giovanni.franchi95@gmail.com>
2023-09-23 16:24:09 +0200

    Add: typologies route

--- src/typologies/typologies.module.ts
+++ src/typologies/typologies.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypologiesService } from './typologies.service';
 import { TypologiesController } from './typologies.controller';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [TypologiesController],
-  providers: [TypologiesService],
+  providers: [TypologiesService, PrismaService],
 })
 export class TypologiesModule {}
